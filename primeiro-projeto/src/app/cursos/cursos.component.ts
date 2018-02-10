57e659ed88c236cced9ae8eeb1f7c6ddf8a37270
Felipe dos S. Assis <felipesntsassis@gmail.com>
2018-02-10 08:18:01 -0200

    Alterações de código da aula 6.

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[] = ['Java', 'ExtJS 4', 'Angular JS'];
+
+  constructor() {
+    this.nomePortal = 'http://a3.io';
+  }
 
   ngOnInit() {
   }
