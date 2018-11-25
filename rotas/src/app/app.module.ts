4724d440163b4eaffcc8c57a7f51daac794553e1
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2018-11-25 16:25:21 -0200

    Inclusão do projeto rotas do módulo Rotas

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { MaterializeModule } from '@samuelberthe/angular2-materialize';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterializeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
