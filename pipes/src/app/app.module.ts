4cd23dcdf1c09969466e77df6a34b29b2047327a
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2018-11-25 12:23:04 -0200

    Aula 45: Internacionalização

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import localePtBr from '@angular/common/locales/pt';
 
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { registerLocaleData } from '@angular/common';
+import { SettingsService } from './settings.service';
 
+registerLocaleData(localePtBr, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -16,7 +20,15 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    // { provide: LOCALE_ID, useValue: 'pt-BR' }
+    // SettingsService
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingService) => settingService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
