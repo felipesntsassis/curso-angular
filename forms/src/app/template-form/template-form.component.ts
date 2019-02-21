85709bf81abac37ed08ee5c1fc95e9ea10df37ef
= <=>
2019-02-20 23:27:35 -0300

    Estudos da aula 78.

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Felipe Assis',
-    email: 'felipesntsassis@gmail.com'
+    nome: null,
+    email: null
   };
 
   constructor() { }
