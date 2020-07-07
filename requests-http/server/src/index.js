071a29c81a923860baa268384ed4f5f8390c8141
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-07-07 07:17:47 -0300

    Aula 136
    Implementação de um servidor node para realizar o upload de arquivos.

---
+++ requests-http/server/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multipart = require('connect-multiparty');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: '*',
+  optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const multipartMiddleware = multipart({ uploadDir: './uploads' });
+app.post('/upload', multipartMiddleware, (req, resp) => {
+  const files = req.files;
+  console.log(files);
+  resp.json({ message: files });
+});
+
+app.use((err, req, resp, next) => {
+  resp.json({ error: err.message });
+});
+
+app.listen(8000, () => {
+  console.log('Servidor porta 8000');
+});
