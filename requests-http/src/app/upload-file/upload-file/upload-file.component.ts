d080b6945c2849f5f688f84ee7e7e01825ce9673
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-07-07 08:21:30 -0300

    Aula 137
    Implementação do upload de arquivos.

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { UploadFileService } from './upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
   styleUrls: ['./upload-file.component.scss']
 })
-export class UploadFileComponent implements OnInit {
+export class UploadFileComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  files: Set<File>;
+  upload$: Subscription;
+
+  constructor(private service: UploadFileService) { }
+
+  ngOnDestroy() {
+    this.upload$.unsubscribe();
+  }
 
   ngOnInit(): void {
   }
@@ -17,12 +28,23 @@ export class UploadFileComponent implements OnInit {
 
     const selectedFiles = event.srcElement.files as FileList;
     const fileNames = [];
+    this.files = new Set();
 
     // tslint:disable-next-line: prefer-for-of
     for (let i = 0; i < selectedFiles.length; i ++) {
       fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
     }
 
     document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
   }
+
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.upload$ = this.service.upload(this.files, 'http://localhost:8000/upload').subscribe(
+        response => console.log('Upload Concluído!')
+      );
+    }
+  }
+
 }
