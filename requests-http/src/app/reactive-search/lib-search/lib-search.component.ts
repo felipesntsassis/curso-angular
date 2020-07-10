2bf38ab46934f0f2e75f7e1ba52498470f417d7a
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-07-09 21:41:49 -0300

    Aula 145
    Implementação de busca reativa utilizando o RxJS.

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -3,7 +3,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
 
 import { Observable } from 'rxjs';
-import { take, tap, map } from 'rxjs/operators';
+import { take, tap, map, filter, distinct, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operators';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-lib-search',
@@ -13,6 +14,7 @@ import { take, tap, map } from 'rxjs/operators';
 export class LibSearchComponent implements OnInit {
 
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  readonly FIELDS = 'name,description,version,homepage';
 
   queryField: FormControl = new FormControl();
   total = 0;
@@ -22,10 +24,27 @@ export class LibSearchComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    // this.results$ = this.queryField.valueChanges.pipe(
+    //   (tap(value => console.log(value)))
+    // );
+    this.results$ = this.queryField.valueChanges.pipe(
+      map(value => value.trim()),
+      filter(value => value.length > 1),
+      debounceTime(200), // tempo necessário para a digitação
+      distinctUntilChanged(),
+      switchMap((value) => this.http.get(this.SEARCH_URL, {
+        params: {
+          search: value,
+          fields: this.FIELDS
+        }
+      })),
+      tap((resp: any) => this.total = resp.total),
+      map((resp: any) => resp.results)
+    );
   }
 
   onSearch() {
-    const fields = 'name,description,version,homepage';
+
     let value = this.queryField.value;
 
     // tslint:disable-next-line: no-conditional-assignment
@@ -33,12 +52,12 @@ export class LibSearchComponent implements OnInit {
 
       const params_ = {
         search: value,
-        fields: fields
+        fields: this.FIELDS
       };
 
       let params = new HttpParams();
       params = params.append('search', value);
-      params = params.append('fields', fields);
+      params = params.append('fields', this.FIELDS);
 
       this.results$ = this.http.get(this.SEARCH_URL, { params }).pipe(
         tap((resp: any) => this.total = resp.total),
